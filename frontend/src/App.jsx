0673745ad14145878f57707fa3cc83aa6f339947
Boureghda Zakari <zakari.boureghda@gmail.com>
2022-05-11 19:23:39 +0200

    Removed aliases

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import Main from "@components/Main";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./styles/App.css";
 
 function App() {
