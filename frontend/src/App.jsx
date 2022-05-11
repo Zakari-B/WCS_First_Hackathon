89f429ada7d1e677179dfe73e0113a884e6ac87d
Boureghda Zakari <zakari.boureghda@gmail.com>
2022-05-11 19:20:34 +0200

    Initial template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Main from "@components/Main";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
