227eca8fc792dbc35e9d21ca2e900075a80e6e05
Crab-Prog <ophelie.coudert@gmail.com>
2022-05-12 19:03:22 +0200

    the Gameover page displays points and a message

--- frontend/src/pages/GameOver.jsx
+++ frontend/src/pages/GameOver.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import PlayerContext from "../contexts/PlayerContext";
+import "../styles/GameOver.css";
 
 const GameOver = () => {
-  return <div>GameOver</div>;
+  let points = 50;
+  let nPoints = -50;
+  let player = "Sacha";
+  const { playerName, setPlayerName } = useContext(PlayerContext);
+
+  return (
+    <>
+      <div className="endContainer">
+        <div className="glassMorphEnd">
+          <h1 className="title-end">Résultat</h1>
+          {points <= 0 ?
+            <div className="text-container">
+              <p className="paragraph-end">Impact de {player} : {points} points.</p>
+              <p className="paragraph-end">“ Bravo, tu as réussi à sauver la Terre ! ”</p>
+              <div>
+                <img src={"https://media.discordapp.net/attachments/973883562820534302/974294353495334953/unknown.png?width=488&height=473"} alt="" className="img-end" />
+              </div>
+            </div>
+            :
+            <div  className="text-container">
+              <p className="paragraph-end">Impact de {player} : {nPoints} points.</p>
+              <p className="paragraph-end">“ Bravo, tu as réussi à détuire la Terre ! ”</p>
+              <div>
+                <img src={"https://cdn.discordapp.com/attachments/973883562820534302/974295504768860180/unknown.png"} alt="" className="img-end" />
+              </div>
+            </div>}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default GameOver;
