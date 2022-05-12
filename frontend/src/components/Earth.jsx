db1c442bc31de77ac6b1a60e80a2e32d0f39c4c0
gnos28 <gnos28@gmail.com>
2022-05-12 15:40:30 +0200

    earth working

--- frontend/src/components/Earth.jsx
+++ frontend/src/components/Earth.jsx
@@ -5,7 +5,7 @@ import EarthDead from "../assets/earth_polluted.png";
 import "../styles/Earth.scss";
 
 const Earth = () => {
-  const [earthHealth, setEarthHealth] = useState(0);
+  const [earthHealth, setEarthHealth] = useState(0); // range [-100 +100]
 
   return (
     <div className="earthContainer">
@@ -13,13 +13,13 @@ const Earth = () => {
         src={EarthGreen}
         alt="EarthGreen"
         className="earthImg earthGreen"
-        style={{ filter: `opacity(${1 - earthHealth})` }}
+        style={{ filter: `opacity(${(earthHealth + 100) / 200})` }}
       />
       <img
         src={EarthDead}
         alt="EarthDead"
         className="earthImg earthDead"
-        style={{ filter: `opacity(${earthHealth})` }}
+        style={{ filter: `opacity(${1 - (earthHealth + 100) / 200})` }}
       />
     </div>
   );
