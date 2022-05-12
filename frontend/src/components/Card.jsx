2477c0719e11ef58138c0be410ceed9b06928223
anthony <anthony.aubert1@orange.fr>
2022-05-12 19:05:15 +0200

    add style.css

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import "../styles/Card.scss";
+import tree from "../assets/svg/tree.svg";
 
 const Card = ({ card }) => {
-	return (
-		<div
-			id={card.id}
-			className={`flex flex-col items-center w-1/5 h-3/4 m-3 border border-slate-700 rounded-md shadow-lg ${
-				card.positif ? "bg-green-200" : "bg-orange-200"
-			} `}
-		>
-			<div className="w-full flex justify-around m-2">
-				<p className="text-2xl">{card.cost}</p>
-				<h1 className="text-2xl font-semibold text-gray-800">{card.name}</h1>
-			</div>
-			<img src="" alt="" className="h-1/3 object-center" />
-			<p className="">{card.text}</p>
-		</div>
-	);
+  return (
+    <div
+      id={card.id}
+      className={`card ${card.positif ? "bg-green-200" : "bg-orange-200"} `}
+    >
+      <div className="card-top">
+        <p className="card-cost">{card.cost}</p>
+        <h1 className="card-title">{card.name}</h1>
+        <div className="card-img">
+          {" "}
+          <img src={tree} alt="" className="card-img" />
+        </div>
+      </div>
+      <div className="card-bottom">
+        <p className="card-effect">{card.text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- frontend/src/components/Hand.jsx
+++ frontend/src/components/Hand.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Energy from "../components/Energy.jsx";
+import Card from "../components/Card.jsx";
 
 const Hand = () => {
+  const sampleCard = {
+    id: 1,
+    name: "Arbre",
+    cost: 1,
+    positif: true,
+    text: "1 degat piochez 2 cartes",
+    value: 1,
+    effect: null,
+    image: ".svg",
+    isStarterDeck: true,
+  };
+
   return (
     <div className="HandInner">
       <Energy />
       <div className="CardContainer">
-        cards go here
+        <Card card={sampleCard} />
       </div>
     </div>
   );
