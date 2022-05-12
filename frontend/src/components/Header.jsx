c97f64f50741ca83636c28db348c45023d4039dc
Crab-Prog <ophelie.coudert@gmail.com>
2022-05-12 20:00:55 +0200

    header with navigation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div>
+        <nav className="full-navigation">
+          <ul className="navigation">
+            <li className="navigation-name">
+              <Link to="/">Jeu</Link>
+            </li>
+            <li className="navigation-name">
+              <Link to="/Global">Global</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default Header;
---
+++ frontend/src/pages/Global.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Global = () => {
+  return (
+    <>
+      <h1>
+          Global informations
+      </h1>
+    </>
+  );
+};
+
+export default Global;
