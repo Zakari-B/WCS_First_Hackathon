1f8e33faa8b445c9e033a11310e6794bf2a6cc63
gnos28 <gnos28@gmail.com>
2022-05-13 06:49:11 +0200

    earth update

--- frontend/src/components/Earth.jsx
+++ frontend/src/components/Earth.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import EarthGreen from "../assets/earth_green.png";
 import EarthDead from "../assets/earth_polluted.png";
+import EarthNeutral from "../assets/earth_neutral.png";
 import gradientBackground from "../assets/gradientBackground.jpg";
 import EarthHealthContext from "../contexts/EarthHealthContext";
 
@@ -19,17 +20,26 @@ const Earth = () => {
       />
       <div className="earthContainer">
         <img
+          src={EarthNeutral}
+          alt="EarthNeutral"
+          className="earthImg earthNeutral"
+          // style={{ filter: `opacity(${(hearthHealth + 50) / 100})` }}
+          draggable={false}
+        />
+        <img
           src={EarthGreen}
           alt="EarthGreen"
           className="earthImg earthGreen"
-          style={{ filter: `opacity(${(hearthHealth + 100) / 200})` }}
+          style={{ filter: `opacity(${Math.max(0, hearthHealth * 4) / 25})` }}
           draggable={false}
         />
         <img
           src={EarthDead}
           alt="EarthDead"
           className="earthImg earthDead"
-          style={{ filter: `opacity(${1 - (hearthHealth + 100) / 200})` }}
+          style={{
+            filter: `opacity(${Math.max(0, 1 - hearthHealth * 4) / 25})`,
+          }}
           draggable={false}
         />
       </div>
