8b95a56c8da9a83b1b28cd33d5b7692c2c3b93ca
anthony <anthony.aubert1@orange.fr>
2022-05-15 12:19:41 +0200

    refacto and convert css to scss GameOver page

--- frontend/src/pages/GameOver.jsx
+++ frontend/src/pages/GameOver.jsx
@@ -2,71 +2,71 @@ import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import PlayerContext from "../contexts/PlayerContext";
 import TurnContext from "../contexts/TurnContext";
-import "../styles/GameOver.css";
+import "../styles/GameOver.scss";
 
 const GameOver = () => {
   const { playerName, playerScore } = useContext(PlayerContext);
   const { setTurn } = useContext(TurnContext);
 
-	useEffect(() => {
-		setTurn(12);
-	}, []);
-	console.log(playerScore);
+  useEffect(() => {
+    setTurn(12);
+  }, []);
+  console.log(playerScore);
 
-	return (
-		<>
-			<div className="endContainer">
-				<div className="glassMorphEnd">
-					<h1 className="title-end text-white">Résultat</h1>
-					{playerScore >= 0 ? (
-						<div className="text-container">
-							<div>
-								<img
-									src={
-										"https://media.discordapp.net/attachments/973883562820534302/974294353495334953/unknown.png?width=488&height=473"
-									}
-									alt=""
-									className="img-end"
-								/>
-							</div>
-							<p className="text-green-500 text-5xl m-4 font-sans">
-								Impact de {playerName} : {playerScore} points.
-							</p>
-							<p className="text-green-500 text-5xl m-4 font-sans">
-								“ Bravo, tu as réussi à sauver la Terre ! ”
-							</p>
-						</div>
-					) : (
-						<div className="text-container">
-							<div>
-								<img
-									src={
-										"https://cdn.discordapp.com/attachments/973883562820534302/974295504768860180/unknown.png"
-									}
-									alt=""
-									className="img-end"
-								/>
-							</div>
-							<p className="text-red-500 text-5xl m-4 font-sans">
-								Impact de {playerName} : {playerScore} points.
-							</p>
-							<p className="text-red-500 text-5xl m-4 font-sans">
-								“ Bravo, tu as réussi à détuire la Terre ! ”
-							</p>
-						</div>
-					)}
-					<Link to="/Global">
-						<button
-							type="submit"
-							className="btn m-2 text-gray-800 py-2 px-8 text-4xl rounded-lg font-bold mx-8 drop-shadow-md pt-4 hover:bg-white/40 hover:text-white"
-						>
-							Voir les scores
-						</button>
-					</Link>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="endContainer">
+        <div className="glassMorphEnd">
+          <h1 className="title-end text-white">Résultat</h1>
+          {playerScore >= 0 ? (
+            <div className="text-container">
+              <div>
+                <img
+                  src={
+                    "https://media.discordapp.net/attachments/973883562820534302/974294353495334953/unknown.png?width=488&height=473"
+                  }
+                  alt=""
+                  className="img-end"
+                />
+              </div>
+              <p className="text-green-500 text-5xl m-4 font-sans">
+                Impact de {playerName} : {playerScore} points.
+              </p>
+              <p className="text-green-500 text-5xl m-4 font-sans">
+                “ Bravo, tu as réussi à sauver la Terre ! ”
+              </p>
+            </div>
+          ) : (
+            <div className="text-container">
+              <div>
+                <img
+                  src={
+                    "https://cdn.discordapp.com/attachments/973883562820534302/974295504768860180/unknown.png"
+                  }
+                  alt=""
+                  className="img-end"
+                />
+              </div>
+              <p className="text-red-500 text-5xl m-4 font-sans">
+                Impact de {playerName} : {playerScore} points.
+              </p>
+              <p className="text-red-500 text-5xl m-4 font-sans">
+                “ Bravo, tu as réussi à détuire la Terre ! ”
+              </p>
+            </div>
+          )}
+          <Link to="/Global">
+            <button
+              type="submit"
+              className="btn m-2 text-gray-800 py-2 px-8 text-4xl rounded-lg font-bold mx-8 drop-shadow-md pt-4"
+            >
+              Voir les scores
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default GameOver;
